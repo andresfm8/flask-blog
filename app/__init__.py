b31dc8a774c289e633fc395be3c4f71c29a3f329
Andres Munevar <afmunevarm@hotmail.com>
2021-07-01 00:21:53 -0400

    Add migration requirements to postgres

--- app/__init__.py
+++ app/__init__.py
@@ -3,24 +3,39 @@ from flask import Flask, make_response, request, redirect, render_template, send
 from dotenv import load_dotenv
 #from firebase_admin import credentials, firestore, initialize_app
 from datetime import datetime
-import json
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from flask.typing import StatusCode
 from werkzeug.security import check_password_hash, generate_password_hash
-from . import db
-from app.db import get_db
 
 load_dotenv()
 app = Flask(__name__)
-app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-db.init_app(app)
 
-#Initialize Firestore DB
-# firebase_creds = json.loads(os.getenv("FIREBASE_CREDS"))
-# cred = credentials.Certificate(firebase_creds)
-# default_app = initialize_app(cred)
-# db = firestore.client()
-# posts_ref = db.collection('posts')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
+    user=os.getenv('POSTGRES_USER'),
+    passwd=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('POSTGRES_HOST'),
+    port=5432,
+    table=os.getenv('POSTGRES_DB'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 @app.route('/')
 def index():
@@ -31,11 +46,8 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
             error = 'Incorrect username.'
@@ -55,24 +67,19 @@ def register():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
 
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
             return f"User {username} created successfully"
         else:
             return error, 418
