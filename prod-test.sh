51ae6cbbdae5e84bfac24a467ef90fd611f49ef8
andresfm8 <afmunevarm@hotmail.com>
2021-07-07 22:56:33 -0400

    Edit endpoint test script

--- prod-test.sh
+++ prod-test.sh
@@ -1,7 +1,7 @@
 # Test 1 - /
-scode=$(curl --write-out %{http_code} --silent --output /dev/null https://sponge-bob.duckdns.org/)
+scode=$(curl --write-out %{http_code} --silent --output /dev/null https://sponge-bob.duckdns.org)
 if [[ "$scode" -ne 200 ]] ; then
-  echo "There was an error. Status: $scode"
+  echo "Test 1 - / There was an error. Status: $scode"
   exit 1
 else
   echo "Test 1 - / is OK"
@@ -9,7 +9,7 @@ fi
 # Test 2 - /health
 scode=$(curl --write-out %{http_code} --silent --output /dev/null https://sponge-bob.duckdns.org/health)
 if [[ "$scode" -ne 200 ]] ; then
-  echo "There was an error. Status: $scode"
+  echo "Test 2 - /health There was an error. Status: $scode"
   exit 1
 else
   echo "Test 2 - /health is OK"
@@ -17,7 +17,7 @@ fi
 # Test 3 - /blog
 scode=$(curl --write-out %{http_code} --silent --output /dev/null https://sponge-bob.duckdns.org/blog)
 if [[ "$scode" -ne 200 ]] ; then
-  echo "There was an error. Status: $scode"
+  echo "Test 4 - /blog There was an error. Status: $scode"
   exit 1
 else
   echo "Test 3 - /blog is OK"
@@ -25,7 +25,7 @@ fi
 # Test 4 - /login
 scode=$(curl --write-out %{http_code} --silent --output /dev/null https://sponge-bob.duckdns.org/login)
 if [[ "$scode" -ne 200 ]] ; then
-  echo "There was an error. Status: $scode"
+  echo "Test 4 - /login There was an error. Status: $scode"
   exit 1
 else
   echo "Test 4 - /login is OK"
@@ -33,7 +33,7 @@ fi
 # Test 5 - /register
 scode=$(curl --write-out %{http_code} --silent --output /dev/null https://sponge-bob.duckdns.org/register)
 if [[ "$scode" -ne 200 ]] ; then
-  echo "There was an error. Status: $scode"
+  echo "Test 5 - /register There was an error. Status: $scode"
   exit 1
 else
   echo "Test 5 - /register is OK"
